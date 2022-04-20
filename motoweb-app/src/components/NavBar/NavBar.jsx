726601f463352111e28b481f405f79575f89a9fb
Lucio Angel <lucio.ivan.angel@gmail.com>
2022-04-20 15:10:02 -0300

    agrego componente NavBar

---
+++ motoweb-app/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavBar.css';
+function NavBar(props) {
+    return (
+        <div>
+            
+            <ul className='nav'>
+                <h1 className='nombre'>Motoweb</h1>
+                <a href='#'>Street</a>
+                <a href='#'>ON/OFF</a>
+                <a href='#'>Scooter</a>
+                
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
